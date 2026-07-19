3e4ac0d617cc610e52c604cef5e77b33779bc9fa
LaoraSad <264025963+LaoraSad@users.noreply.github.com>
2026-07-18 19:07:24 -0500

    chore(users): remove LogoutForm (no longer needed)

--- apps/users/forms/logout_form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class LogoutForm(forms.Form):
-    confirm = forms.BooleanField(
-        initial=True,
-        widget=forms.HiddenInput,
-        required=False,
-    )
